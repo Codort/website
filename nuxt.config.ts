dfadf3ad3690b018fe780c0774476169f818cfad
Jamie Slome <jamieizakslome@gmail.com>
2024-07-03 21:06:40 +0100

    style: add tailwindcss as nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  compatibilityDate: "2024-07-03"
+  compatibilityDate: "2024-07-03",
+  modules: ["@nuxtjs/tailwindcss"]
 })
