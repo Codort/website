567d6737dc920fb7a6012b454714dbb75d3b25ce
Raphael Sonabend <raphaelsonabend@gmail.com>
2024-10-10 09:38:47 +0100

    update ptshp txt
    DCO signed and agreed by: Raphael Sonabend <raphaelsonabend@gmail.com>

--- data/partnerships.js
+++ data/partnerships.js
@@ -3,18 +3,18 @@ export default [
     id: 'marketplace-partner',
     name: 'Marketplace Partner',
     description:
-      'Join our Marketplace Partner program to showcase your software on our platform. We actively promote your product to our extensive network, driving visibility and sales. Benefit from increased exposure and a streamlined path to new customers through our dedicated marketing efforts.',
+      'Showcase your tools for source code management on the Codort marketplace and benefit from increased exposure. As a Marketplace Partner we will prominently feature your product on our marketplace as well as including your tool in our marketing materials.',
   },
   {
     id: 'integration-partner',
     name: 'Integration Partner',
     description:
-      'Our Integration Partner program allows your software to be integrated directly into our platform. This enables your customers to effortlessly incorporate your solution into their existing tech stack, enhancing usability and satisfaction. Leverage our infrastructure to provide a seamless experience for your users.',
+      'Join Codort as an Integration Partner and we will work with you to seamlessly integrate your tool into Codort. Once integrated, customers can effortlessly incorporate your solution into their existing tech stack, enhancing accessibility and satisfaction.',
   },
   {
     id: 'whitelabel-partner',
     name: 'White Label Partner',
     description:
-      'Our White Label Partner program enables you to integrate our software into your backend and rebrand it as your own. This allows you to offer a tailored solution under your brand, expanding your product offerings without the need for additional development resources.',
+      'Fed up of managing integrations for source code management tools yourself? Become a White Label Partner and integrate Codort into your backend with your own branding. Seamlessly add and manage integrations without the hassle of maintaining them yourself.',
   },
 ];
