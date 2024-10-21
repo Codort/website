6073381eda51e0c346fd79178c44bb875466653a
Raphael Sonabend <raphaelsonabend@gmail.com>
2024-10-21 09:49:07 +0100

    return hacky fix
    Signed-off-by: Raphael Sonabend <raphaelsonabend@gmail.com>

--- components/Header.vue
+++ components/Header.vue
@@ -55,13 +55,17 @@
                 <!-- </div> -->
               </div>
               <div class="flex items-center justify-end pb-3 sm:pb-2">
-                <UButton
-                  :icon="isDark ? 'i-fa-moon-o' : 'i-fa-sun-o'"
-                  variant="ghost"
-                  class="text-black dark:text-white"
-                  aria-label="Change colour mode"
-                  @click="toggleColourMode"
-                ></UButton>
+                <ColorScheme placeholder="" tag="span">
+                  <UButton
+                    :icon="isDark ? 'i-fa-moon-o' : 'i-fa-sun-o'"
+                    variant="ghost"
+                    class="text-black dark:text-white"
+                    aria-label="Change colour mode"
+                    @click="toggleColourMode"
+                  ></UButton>
+                </ColorScheme>
+                <Icon name="fa:sun-o" class="w-0 h-0"></Icon>
+                <Icon name="fa:moon-o" class="w-0 h-0"></Icon>
               </div>
             </div>
             <div class="hidden sm:block left-0 font-mark pb-6">
